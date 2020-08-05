4d9d505530c8aaa7abb7cf082cf7c6340c53a96a
Jonathan Kim <jnthnk@outlook.com>
2020-08-05 17:39:37 +0200

    Refactor pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,27 @@
-// 
+//
 import type { AppProps } from 'next/app'
 
-// 
+//
 import GlobalStyle from 'components/GlobalStyle'
 
-// 
+//
+import { ThemeProvider } from 'styled-components'
 
+//
+import { SWrap } from 'components/Blocks'
+
+//
 import Header from 'components/Header'
 
-// 
+//
 
 const App = ({ Component, pageProps }: AppProps) => (
 	<>
 		<GlobalStyle />
 		<Header />
-		<Component {...pageProps} />
+		<SWrap as="main">
+			<Component {...pageProps} />
+		</SWrap>
 	</>
 )
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,29 @@
+//
+import Welcome from 'components/Welcome'
+
+// 
+const cards = [
+	{
+		id: 1,
+		name: 'Bete Empadinhas'
+	},
+	{
+		id: 2,
+		name: 'Bordando Cores'
+	},
+	{
+		id: 3,
+		name: 'Geladinhos Gourmet'
+	}
+]
+
+// 
 const Home = () => (
-	<h1>Início</h1>
+	<Welcome
+		title="Guia de empresas, produtos e serviços em Foz do Iguaçu"
+		description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos tempore deleniti repudiandae nam vel? Cum recusandae veniam itaque, magni dolor ipsum nobis, corporis eligendi."
+		cards={cards}
+	/>
 )
 
 export default Home
