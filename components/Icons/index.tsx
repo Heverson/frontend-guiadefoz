9d9a0d5203c4c6c8e10173d749a007fd52e5a034
Jonathan Kim <jnthnk@outlook.com>
2020-08-07 01:28:42 +0200

    Add Icons components

---
+++ components/Icons/index.tsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+import { StyledIconBase } from '@styled-icons/styled-icon'
+import {
+	FacebookSquare,
+	Instagram,
+	Discord
+} from '@styled-icons/boxicons-logos'
+
+const SIcon = styled(StyledIconBase)`
+	display: block;
+	width: 1em;
+	height: 1em;
+`
+const SFacebook = styled(FacebookSquare)``
+const SInstagram = styled(Instagram)``
+const SDiscord = styled(Discord)``
+
+export {
+	SIcon,
+	SFacebook,
+	SInstagram,
+	SDiscord
+}
