b4a1bc715a5311233f621d1c772a3c7ad217ae5a
Jonathan Kim <jnthnk@outlook.com>
2020-08-07 01:28:25 +0200

    Refactor page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,22 @@
-// 
 import type { AppProps } from 'next/app'
-
-// 
 import GlobalStyle from 'components/GlobalStyle'
-
-// 
-import { ThemeProvider } from 'styled-components'
-
-// 
-import { SWrap } from 'components/Blocks'
-
-// 
+import styled, { ThemeProvider } from 'styled-components'
 import Header from 'components/Header'
 import Footer from 'components/Footer'
 
-//
+const SMain = styled.main`
+	overflow: hidden;
+	position: relative;
+	z-index: 1;
+`
 
 const App = ({ Component, pageProps }: AppProps) => (
 	<>
 		<GlobalStyle />
 		<Header />
-		<SWrap as="main">
+		<SMain>
 			<Component {...pageProps} />
-		</SWrap>
+		</SMain>
 		<Footer />
 	</>
 )
