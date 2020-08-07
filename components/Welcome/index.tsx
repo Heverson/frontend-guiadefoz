0aacf164ccc9c7d78cb406b0b29e5143d495271c
Jonathan Kim <jnthnk@outlook.com>
2020-08-08 00:04:56 +0200

    Refactor Welcome

--- components/Welcome/index.tsx
+++ components/Welcome/index.tsx
@@ -10,6 +10,7 @@ import {
 	SButton
 } from 'components/Blocks'
 import Card, { SCard } from 'components/Card'
+import { useFormik } from 'formik'
 
 const SForm = styled.form`
 	> ${SItems} {
@@ -56,48 +57,61 @@ const SWelcome = styled.header`
 	}
 `
 
-const Welcome = ({ title, description, cards }: IWelcome) => (
-	<SWelcome>
-		<SWrap isCenter spacing="large">
-			<STitle size="xlarge">{title}</STitle>
-			<SText size="large">{description}</SText>
-			<SForm>
-				<SItems spacing="normal">
-					<SItem>
-						<SLabel htmlFor="termo">
-							Palavra-chave
-						</SLabel>
-						<SField id="termo" name="termo" type="text" required />
-					</SItem>
-					<SItem>
-						<SLabel htmlFor="categoria">
-							Categoria
-						</SLabel>
-						<SField id="categoria" name="categoria" type="text" />
-					</SItem>
-					<SItem>
-						<SLabel htmlFor="regiao">
-							Regiao e bairro
-						</SLabel>
-						<SField id="regiao" name="regiao" type="text" />
-					</SItem>
-					<SItem>
-						<SButton as="button" type="submit">
-							Buscar
-						</SButton>
-					</SItem>
+const Welcome = ({ title, description, cards }: IWelcome) => {
+	const formik = useFormik({
+		initialValues: {
+			termo: '',
+			categoria: '',
+			regiao: ''
+		},
+		onSubmit: values => {
+			console.log(JSON.stringify(values, null, 2))
+		}
+	})
+	return (
+		<SWelcome>
+			<SWrap isCenter spacing="large">
+				<STitle size="xlarge">{title}</STitle>
+				<SText size="large">{description}</SText>
+				<SForm onSubmit={formik.handleSubmit}>
+					<SItems spacing="normal">
+						<SItem>
+							<SLabel htmlFor="termo">Palavra-chave</SLabel>
+							<SField
+								id="termo"
+								name="termo"
+								type="text"
+								value={formik.values.termo}
+								onChange={formik.handleChange}
+								required
+							/>
+						</SItem>
+						<SItem>
+							<SLabel htmlFor="categoria">Categoria</SLabel>
+							<SField id="categoria" name="categoria" type="text" value={formik.values.termo} onChange={formik.handleChange} />
+						</SItem>
+						<SItem>
+							<SLabel htmlFor="regiao">Regiao e bairro</SLabel>
+							<SField id="regiao" name="regiao" type="text" />
+						</SItem>
+						<SItem>
+							<SButton as="button" type="submit">
+								Buscar
+							</SButton>
+						</SItem>
+					</SItems>
+				</SForm>
+				<SItems spacing="xlarge">
+					{cards.map(card => (
+						<SItem key={card.id}>
+							<Card title={card.name} rating={5} region="CE" />
+						</SItem>
+					))}
 				</SItems>
-			</SForm>
-			<SItems spacing="xlarge">
-				{cards.map(card => (
-					<SItem key={card.id}>
-						<Card title={card.name} rating={5} region="CE" />
-					</SItem>
-				))}
-			</SItems>
-		</SWrap>
-	</SWelcome>
-)
+			</SWrap>
+		</SWelcome>
+	)
+}
 
 interface IWelcome {
 	title: string
