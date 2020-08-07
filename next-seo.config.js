065ab991f261220d9322010f89bbd7e65280bcf1
Jonathan Kim <jnthnk@outlook.com>
2020-08-07 12:32:02 +0200

    Add default SEO config

---
+++ next-seo.config.js
@@ -0,0 +1,10 @@
+export default {
+	title: 'Guia de Foz',
+	description: 'Guia comercial da cidade de Foz do Iguaçu',
+	openGraph: {
+		type: 'website',
+		locale: 'pt_BR',
+		url: 'https://www.guia.foz.br',
+		site_name: 'Guia de Foz'
+	}
+}
