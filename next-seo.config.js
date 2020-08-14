d7b6088650d09b1070f33175e2aab63752803eba
Jonathan Kim <jnthnk@outlook.com>
2020-08-15 00:48:56 +0200

    Re-format configs

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,10 +1,10 @@
 export default {
-	title: 'Guia de Foz',
-	description: 'Guia comercial da cidade de Foz do Iguaçu',
-	openGraph: {
-		type: 'website',
-		locale: 'pt_BR',
-		url: 'https://www.guia.foz.br',
-		site_name: 'Guia de Foz'
-	}
+  title: 'Guia de Foz',
+  description: 'Guia comercial da cidade de Foz do Iguaçu',
+  openGraph: {
+    type: 'website',
+    locale: 'pt_BR',
+    url: 'https://www.guia.foz.br',
+    site_name: 'Guia de Foz'
+  }
 }
